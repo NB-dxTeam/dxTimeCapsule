7e974451bb1ca481a30697c84437b323df763db7
yeongnote <eongh0640@gmail.com>
2024-02-24 17:59:58 +0900

    [Feat] 콜렉션 뷰 추가, 레이아웃 설정, 콜렉션 뷰 셀 추가.

--- dxTimeCapsule/CapsuleMapViewController.swift
+++ dxTimeCapsule/CapsuleMapViewController.swift
@@ -8,15 +8,143 @@
 import UIKit
 import NMapsMap
 import CoreLocation
-
+import SnapKit
 
 class CapsuleMapViewController: UIViewController {
+    // 임시 네비게이션 바
+    private lazy var nvBar: UINavigationBar = {
+        let bar = UINavigationBar()
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
+    private lazy var capsuleMap: NMFMapView = {
+        let map = NMFMapView()
+        
+        return map
+    }()
+    private var capsuleCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        //layout.scrollDirection = .horizontal
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collection.backgroundColor = .systemBlue
+        return collection
+    }()
+    private let dragBar: UIView = {
+        let bar = UIView()
+        
+        return bar
+    }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        let mapView = NMFMapView(frame: view.bounds)
-        view.addSubview(mapView)
+        addSubViews()
+        autoLayouts()
+        viewDidLayoutSubviews()
+        configCV()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        capsuleCollectionView.layer.cornerRadius = 30
+        capsuleCollectionView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        capsuleCollectionView.layer.masksToBounds = true
+    }
+    
+    private func configCV() {
+        capsuleCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        capsuleCollectionView.delegate = self
+        capsuleCollectionView.dataSource = self
+        capsuleCollectionView.register(LockedCapsuleCell.self, forCellWithReuseIdentifier: LockedCapsuleCell.identifier)
         
+        if let layout = capsuleCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.itemSize = CGSize(width: view.frame.width - 48, height: 120)
+            layout.minimumLineSpacing = 12
+            layout.sectionInset = UIEdgeInsets(top: 12, left: 24, bottom: 12, right: 24)
+        }
     }
 }
+
+extension CapsuleMapViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7 // 임시 설정.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LockedCapsuleCell", for: indexPath) as? LockedCapsuleCell else {fatalError("Unable to dequeue CapsuleCollectionViewCell")}
+        
+        return cell
+    }
+    
+}
+
+//extension CapsuleMapViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        
+//    }
+//}
+
+extension CapsuleMapViewController {
+    private func addSubViews() {
+        self.view.addSubview(nvBar)
+        self.view.addSubview(capsuleMap)
+        self.view.addSubview(capsuleCollectionView)
+        self.view.addSubview(dragBar)
+    }
+    private func autoLayouts() {
+        nvBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        capsuleMap.snp.makeConstraints { make in
+            make.top.equalTo(nvBar.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(capsuleCollectionView.snp.top)
+            
+        }
+        
+        capsuleCollectionView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.height.equalTo(350)
+        }
+        
+        dragBar.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(12)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(60)
+            make.height.equalTo(8)
+        }
+    }
+}
+
+
+// MARK: - Preview
+import SwiftUI
+
+struct PreView: PreviewProvider {
+    static var previews: some View {
+        CapsuleMapViewController().toPreview()
+    }
+}
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
