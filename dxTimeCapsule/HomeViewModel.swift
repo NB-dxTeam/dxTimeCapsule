346bab0eb12f04bbb6d6a919d236b56330940727
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-02-24 20:56:57 +0900

    [feat]UI구성(열어볼타임캡슐)

---
+++ dxTimeCapsule/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  dxTimeCapsule
+//
+//  Created by t2023-m0028 on 2/23/24.
+//
+
+import Foundation
---
+++ dxTimeCapsule/UpcomingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UpcomingCollectionViewCell.swift
+//  dxTimeCapsule
+//
+//  Created by t2023-m0028 on 2/24/24.
+//
+
+import UIKit
+
+class UpcomingCollectionViewCell: UICollectionViewCell {
+    
+}
