eb34babf88db9ee34824eed2ddfada4a48aa8078
Jooyeong(Yugen) <supershicker@gmail.com>
2024-03-24 01:40:49 +0900

    삭제(LocationHistoryViewModel.swift, MainCapsuleViewModel.swift): 사용되지 않는 파일 삭제 피트(TimeBoxViewModel.swift, DynamicIslandManager.swift): 새로운 파일 추가

---
+++ dxTimeCapsule/TimeBoxViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TimeBoxViewModel.swift
+//  dxTimeCapsule
+//
+//  Created by t2023-m0031 on 3/24/24.
+//
+
+import Foundation
---
+++ dxTimeCapsule/Utilities/Notification/DynamicIslandManager.swift
@@ -0,0 +1,8 @@
+//
+//  DynamicIslandManager.swift
+//  dxTimeCapsule
+//
+//  Created by t2023-m0031 on 3/24/24.
+//
+
+import Foundation
